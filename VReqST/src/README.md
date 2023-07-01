c8e116ed89e4bb6337d06852351f4acef135f266
vranonymous <110216998+vranonymous@users.noreply.github.com>
2023-07-01 21:56:33 +0530

    Add files via upload

---
+++ VReqST/src/README.md
@@ -0,0 +1,19 @@
+# VReqST
+
+### Steps to execute
+
+- Make sure you have Node.js v14 installed, some packaged might not be compatible with other versions.
+
+```sh
+-|> Go inside the VReqST-main folder
+-|> Now again go inside the VReqST folder
+-|> Open Terminal And Run
+     $ npm install && npm run client-install
+     $ npm run dev
+-|> Now to start the validation_server on Port 5000 . Again,Go Inside the Validation_server folder
+-|> Open the terminal and Run
+     $ npm install
+     $ nodemon index.js
+```
+
+to start the local server at http://localhost:3000/ and Validation_server at http://localhost:5001
---
+++ VReqST/src/server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const usersRouter = require("./routes/api/users");
+const JsonRouter = require("./routes/api/json");
+const apiprocessor = require("./routes/api/apiprocessroutes")
+const ProjectRouter = require("./routes/api/project");
+const config = require("config");
+var cors = require("cors");
+const app = express();
+// Body parser middleware
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+app.use(express.json());
+app.use(cors());
+
+// DB Config
+const db = config.get("mongoURI");
+// Connect to MongoDB
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch((err) => console.log(err));
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
+// Routes
+app.use("/api/users", usersRouter);
+app.use("/api/json", JsonRouter);
+app.use("/api/project", ProjectRouter);
+app.use("/api",apiprocessor)
+
+const port = process.env.PORT || 5002;
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
